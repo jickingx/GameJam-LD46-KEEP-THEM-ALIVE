8222d99c2b9cc219c0d697af4250044ccccbde91
jickingx <jickingx@gmail.com>
2020-04-19 01:55:17 +0800

    update card state validation switch scene on win and lose

--- SCENES/GameBoard.gd
+++ SCENES/GameBoard.gd
@@ -3,17 +3,33 @@ extends Node2D
 const MARGIN = 14
 const PADDING = 4
 const GRID_SIZE = 128
-
+const max_cards_h = 6
+const max_cards_v = 4
 const Card = preload("res://Items/Card.tscn")
+const EndScene = preload("res://Items/Card.tscn")
+
+
 var rng = RandomNumberGenerator.new()
+var max_evil_card = Global.evil_card_count
 
 func _ready():
 	spawn_cards_to_board()
+	$CanvasLayer/LabelInfectedCount.text = str(max_evil_card)
+
+
+func _on_Button_button_up():
+	process_results()
+
+
+func _on_Button_quarantine_timeout():
+	process_results()
+
+
+func _on_Popup_Button_button_up():
+	get_tree().reload_current_scene()
+
 
 func spawn_cards_to_board():
-	var max_cards_h = 6
-	var max_cards_v = 4
-	var max_evil_card = 12
 	var card_counter = 0
 	var evil_card_indices = get_random_set(
 			max_evil_card,
@@ -68,15 +84,22 @@ func validate_selected_cards():
 	for i in cards.size() :
 		if not cards[i].validate():
 			invalid_counter += 1
+	if invalid_counter > 0:
+		return false
+	return true
 
 
-func _on_Button_button_up():
-	validate_selected_cards()
+func process_results():
+	if validate_selected_cards() :
+		Global.evil_card_count -= 1
+	else:
+		Global.evil_card_count += Global.evil_card_count / 2
+	
+	if Global.evil_card_count <= 0 || Global.evil_card_count >= (max_cards_h * max_cards_v):
+		get_tree().change_scene("res://SCENES/End.tscn")
 	
 	#show pop up if success
 	$CanvasLayer/Button.disabled = true
 	$CanvasLayer/PopupDialog.popup()
 
 
-func _on_Button_quarantine_timeout():
-	validate_selected_cards()
